84eee854f1bfeeb6b1d3be1bffdb8ff327106a4a
ninjasan <pmathur2@gmail.com>
2015-09-08 22:23:41 -0700

    Updating table references
    fix: matches table now references players
    
    Updating the player Ids in the matches table to reference the ids in the
    players table. That way, there can't be a match with a player that
    doesn't exist.

--- tournament.sql
+++ tournament.sql
@@ -7,16 +7,15 @@
 -- these lines here.
 
 -- Create the tournament database, if it doesn't already exist
--- Comment this line out of you don't need to recreate the database
+-- Comment this line out of you don't need to create\recreate the database
 --CREATE DATABASE tournament;
 
 -- Now that the database exists, connect to it
 \c tournament;
 
 -- Drop the all the tables, to start from a clean slate.
-DROP TABLE tournaments;
-DROP TABLE players;
 DROP TABLE matches;
+DROP TABLE players;
 
 -- Create the players table.
 -- This table is used to track all the players in each of the tournaments
@@ -40,8 +39,8 @@ CREATE TABLE players (id serial primary key,
 --     player_2_id: the unique id of the second player in this match
 --     match_result: the id of the winner in this match
 CREATE TABLE matches (  round integer,
-                        player_1_id integer,
-                        player_2_id integer,
+                        player_1_id integer references players (id),
+                        player_2_id integer references players (id),
                         match_result integer,
                         primary key (round, player_1_id, player_2_id)
                      );
